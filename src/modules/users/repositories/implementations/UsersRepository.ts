e9ad12cfcb5247d6bcef8ea477f7c1c2ac276426
Lemuel Zara <lemuel.czara@gmail.com>
2021-03-23 17:21:02 -0300

    feat: create user

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -19,7 +19,16 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+
+    user.name = name;
+    user.email = email;
+    user.created_at = new Date();
+    user.updated_at = new Date();
+
+    this.users.push(user);
+
+    return user;
   }
 
   findById(id: string): User | undefined {
